0c6ef18a1e5f5b4d2d138a593aa273ff1ce0cda9
Viktor Petersson <vpetersson@wireload.net>
2013-09-07 13:53:54 -0700

    Added handling for OS X to only bind on localhost.

--- server.py
+++ server.py
@@ -1,9 +1,19 @@
 import os
+import platform
+
 from bottle import route, run
 
+# Avoid binding on public OS on OS X.
+bind = None
+if 'Darwin' in platform.platform():
+    bind = '127.0.0.1'
+else:
+    bind = '0.0.0.0'
 
-@route("/")
+
+@route('/')
 def hello_world():
-    return "Hello World!"
+    return 'Hello World!'
 
-run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+run(host=bind, port=int(os.environ.get('PORT', 5000)))
